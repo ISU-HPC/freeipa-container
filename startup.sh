9e2ddaf3d0072ac6be238b0c179153654dd6cf0f
David Orman <orman@iastate.edu>
2020-01-15 10:00:23 -0600

    switching to startup.sh for initial init

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#  This file is called from the init script and intended to be mounted over to start services, 
+#  install additional software, etc without the need to rebuild the image everytime.
+#
+
+echo "Hi from the startup file"
+
