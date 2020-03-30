7c3ce7b6e80b930cfddc843fe7df47266fca4d6b
David Orman <orman@iastate.edu>
2020-03-30 15:00:06 -0500

    radius startup moves

--- startup.sh
+++ startup.sh
@@ -4,5 +4,13 @@
 #  install additional software, etc without the need to rebuild the image everytime.
 #
 
-echo "Hi from the startup file"
-
+echo "Hi from the /usr/local/bin/startup.sh file"
+mkdir -p -m 755 /var/run/munge
+mkdir -p -m 700 /var/log/munge
+chown munge /var/run/munge /var/log/munge
+chgrp munge /var/run/munge /var/log/munge
+/sbin/sshd
+/sbin/munged -f
+/sbin/crond
+/usr/libexec/postfix/master -w
+echo "startup.sh complete"
